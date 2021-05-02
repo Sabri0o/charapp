b54207d2f66d54d417599c28967455ccca82456e
Sabri0o <sabritrabelsi90@gmail.com>
2021-05-02 15:34:22 +0100

    creating server and serving static assets

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//using the set method to assign pug as the view engine property's value of express
+app.set("view engine", "pug");
+
+// serving static assets
+app.use('/public', express.static(__dirname + '/public'));
+
+//rendering the home page from the response.
+app.get("/", function (req, res) {
+  // console.log(__dirname)
+  res.render(__dirname + "/views/pug/home.pug");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
