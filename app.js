3901b3c2c206091917cb6ad63722ab717ba8a9c9
Sabri0o <sabritrabelsi90@gmail.com>
2021-05-04 00:58:22 +0100

    serialization and deserialization

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-require('dotenv').config()
-require('./database')
-const express = require("express");
-const session = require("express-session")
-const passport = require("passport")
-const app = express();
+// Important: In MongoDB, a database is not created until it gets content!
 
+require("dotenv").config();
+const myDatabase = require("./connection");
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+
+// To make a query search for a Mongo _id, you will have to create
+//const ObjectID = require('mongodb').ObjectID;, and then to use it you call new ObjectID(THE_ID)
+const ObjectID = require("mongodb").ObjectID;
+
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,34 +19,58 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "pug");
 
 // serving static assets
-app.use('/public', express.static(__dirname + '/public'));
+app.use("/public", express.static(__dirname + "/public"));
 
-const mySecret = process.env['SESSION_SECRET']
+const mySecret = process.env["SESSION_SECRET"];
 
 // set up the session settings
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 
 //passport.initialize() is a middle-ware that initialises Passport.
-app.use(passport.initialize())
+app.use(passport.initialize());
 
-//passport.session() is middleware that alters the request object and change the session id (from the client cookie) 
+//passport.session() is middleware that alters the request object and change the session id (from the client cookie)
 //into the true deserialized user object.
 
-app.use(passport.session())
+app.use(passport.session());
 
+myDatabase(async (client) => {
+  const myDatabase = await client.db("charApp").collection("users");
 
-
-//rendering the home page from the response.
-app.get("/", function (req, res) {
-  // console.log(__dirname)
-  res.render(__dirname + "/views/pug/home.pug",{message_1:'Hello there',message_2:'nice to meet you'});
-});
+  //rendering the home page from the response.
+  app.get("/", function (req, res) {
+    // console.log(__dirname)
+    res.render(__dirname + "/views/pug/home.pug", {
+      message_1: "Hello there, nice to meet you",
+      message_2: "please log in",
+    });
+  });
+  //Serialization
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+  //deserialization
+  passport.deserializeUser((id, done) => {
+    myDatabase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+}).catch(error=>{
+  app.get('/',(req,res)=>{
+    res.render(__dirname + "/views/pug/home.pug", {
+      message_1: "error",
+      message_2: "unable log in",
+    });
+  })
+})
 
 app.listen(8000, () => {
-  console.log("Listening on port 3000");
+  console.log("Listening on port 8000");
 });
---
+++ connection.js
@@ -0,0 +1,32 @@
+// this link is helpful https://www.mongodb.com/blog/post/quick-start-nodejs-mongodb--how-to-get-connected-to-your-database  
+// Important: In MongoDB, a database is not created until it gets content! (really important)
+
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+
+const URI = process.env["MONGO_URI"];
+
+const main = async function (callback) {
+  const client = new MongoClient(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  try {
+      await client.connect()
+      await listDatabases(client); // list all databases
+      await callback(client)
+  } catch (error) {
+      console.log(error)
+      throw new Error('unable to connect to database')
+  }
+};
+
+// function to list all databases in the cluster
+const listDatabases = async function(client){
+  const databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+module.exports = main
